996d4ec5bd5a11ee6dc1705235fab2ad2746cf82
Meelis Lutsar <31948961+Kuuskinen@users.noreply.github.com>
2017-11-17 18:36:21 +0200

    See on toorik

---
+++ main.php
@@ -0,0 +1,16 @@
+<?php
+//Siia tuleb PHP kood.
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+   <meta charset="utf-8">
+   <title>OSIRIS-REx</title>
+</head>
+<body>
+<h1>See on alustus</h1>
+<p>See on esialgne tekst.</p>
+
+</body>
+</html>
